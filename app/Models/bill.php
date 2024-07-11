eafed3a6b4a160447d0ddaf49d6bc745d7667dac
gharraahaddad <167914735+gharraahaddad@users.noreply.github.com>
2024-07-12 00:35:23 +0300

    editing in relations

--- app/Models/bill.php
+++ app/Models/bill.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class bill extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+    public function users()
+    {
+        $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
+
--- app/Models/report.php
+++ app/Models/report.php
@@ -9,4 +9,8 @@ class report extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    public function users()
+{
+    $this->belongsTo(User::class, 'user_id', 'id');
+}
 }
